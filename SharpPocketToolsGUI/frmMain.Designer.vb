1a3b0e3d570ce09c1667df8bdd34ec4ff021a59e
SilverGreen93 <mihai.vasiliu.93@gmail.com>
2024-01-01 16:58:55 +0200

    Add project files.

---
+++ SharpPocketToolsGUI/frmMain.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        txtLog = New TextBox()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        GroupBox2 = New GroupBox()
+        optWav = New RadioButton()
+        optMp3 = New RadioButton()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        optRename = New RadioButton()
+        optOverwrite = New RadioButton()
+        cmbPcModel = New ComboBox()
+        Label3 = New Label()
+        GroupBox3 = New GroupBox()
+        txtFileName = New TextBox()
+        optFromFilename = New RadioButton()
+        optUseFilename = New RadioButton()
+        linkWeb = New LinkLabel()
+        Label2 = New Label()
+        GroupBox2.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' txtLog
+        ' 
+        txtLog.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        txtLog.Font = New Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(204))
+        txtLog.ForeColor = Color.White
+        txtLog.Location = New Point(12, 136)
+        txtLog.Multiline = True
+        txtLog.Name = "txtLog"
+        txtLog.ScrollBars = ScrollBars.Vertical
+        txtLog.Size = New Size(807, 391)
+        txtLog.TabIndex = 5
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(optWav)
+        GroupBox2.Controls.Add(optMp3)
+        GroupBox2.Location = New Point(12, 44)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(166, 60)
+        GroupBox2.TabIndex = 9
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Audio output format"
+        ' 
+        ' optWav
+        ' 
+        optWav.AutoSize = True
+        optWav.Checked = True
+        optWav.Location = New Point(17, 26)
+        optWav.Name = "optWav"
+        optWav.Size = New Size(61, 24)
+        optWav.TabIndex = 1
+        optWav.TabStop = True
+        optWav.Text = "WAV"
+        optWav.UseVisualStyleBackColor = True
+        ' 
+        ' optMp3
+        ' 
+        optMp3.AutoSize = True
+        optMp3.Location = New Point(84, 26)
+        optMp3.Name = "optMp3"
+        optMp3.Size = New Size(59, 24)
+        optMp3.TabIndex = 0
+        optMp3.TabStop = True
+        optMp3.Text = "MP3"
+        optMp3.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 113)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(85, 20)
+        Label1.TabIndex = 10
+        Label1.Text = "Log output:"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(optRename)
+        GroupBox1.Controls.Add(optOverwrite)
+        GroupBox1.Location = New Point(195, 44)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(218, 60)
+        GroupBox1.TabIndex = 12
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "File overwrite policy"
+        ' 
+        ' optRename
+        ' 
+        optRename.AutoSize = True
+        optRename.Checked = True
+        optRename.Location = New Point(19, 26)
+        optRename.Name = "optRename"
+        optRename.Size = New Size(84, 24)
+        optRename.TabIndex = 1
+        optRename.TabStop = True
+        optRename.Text = "Rename"
+        optRename.UseVisualStyleBackColor = True
+        ' 
+        ' optOverwrite
+        ' 
+        optOverwrite.AutoSize = True
+        optOverwrite.Location = New Point(109, 26)
+        optOverwrite.Name = "optOverwrite"
+        optOverwrite.Size = New Size(94, 24)
+        optOverwrite.TabIndex = 0
+        optOverwrite.Text = "Overwrite"
+        optOverwrite.UseVisualStyleBackColor = True
+        ' 
+        ' cmbPcModel
+        ' 
+        cmbPcModel.FormattingEnabled = True
+        cmbPcModel.Items.AddRange(New Object() {"1150", "1211", "1245", "1248", "1251", "1261", "1280", "1350", "1360", "1401", "1402", "1403", "1421", "1425", "1430", "1445", "1450", "1460", "1475", "1500", "1600", "E220", "G850"})
+        cmbPcModel.Location = New Point(136, 6)
+        cmbPcModel.Name = "cmbPcModel"
+        cmbPcModel.Size = New Size(76, 28)
+        cmbPcModel.TabIndex = 13
+        cmbPcModel.Text = "1211"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 9)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(118, 20)
+        Label3.TabIndex = 14
+        Label3.Text = "Sharp PC model:"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(txtFileName)
+        GroupBox3.Controls.Add(optFromFilename)
+        GroupBox3.Controls.Add(optUseFilename)
+        GroupBox3.Location = New Point(430, 44)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(389, 60)
+        GroupBox3.TabIndex = 13
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Sharp audio recording file name"
+        ' 
+        ' txtFileName
+        ' 
+        txtFileName.Location = New Point(214, 23)
+        txtFileName.MaxLength = 7
+        txtFileName.Name = "txtFileName"
+        txtFileName.Size = New Size(169, 27)
+        txtFileName.TabIndex = 2
+        ' 
+        ' optFromFilename
+        ' 
+        optFromFilename.AutoSize = True
+        optFromFilename.Checked = True
+        optFromFilename.Location = New Point(19, 26)
+        optFromFilename.Name = "optFromFilename"
+        optFromFilename.Size = New Size(126, 24)
+        optFromFilename.TabIndex = 1
+        optFromFilename.TabStop = True
+        optFromFilename.Text = "From filename"
+        optFromFilename.UseVisualStyleBackColor = True
+        ' 
+        ' optUseFilename
+        ' 
+        optUseFilename.AutoSize = True
+        optUseFilename.Location = New Point(151, 26)
+        optUseFilename.Name = "optUseFilename"
+        optUseFilename.Size = New Size(57, 24)
+        optUseFilename.TabIndex = 0
+        optUseFilename.Text = "Use:"
+        optUseFilename.UseVisualStyleBackColor = True
+        ' 
+        ' linkWeb
+        ' 
+        linkWeb.AutoSize = True
+        linkWeb.Location = New Point(621, 9)
+        linkWeb.Name = "linkWeb"
+        linkWeb.Size = New Size(198, 20)
+        linkWeb.TabIndex = 15
+        linkWeb.TabStop = True
+        linkWeb.Text = "Visit project page on GitHub"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(240, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(313, 20)
+        Label2.TabIndex = 16
+        Label2.Text = "Drag && Drop bas, wav or mp3 files to convert."
+        ' 
+        ' frmMain
+        ' 
+        AllowDrop = True
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(831, 539)
+        Controls.Add(Label2)
+        Controls.Add(linkWeb)
+        Controls.Add(GroupBox3)
+        Controls.Add(Label3)
+        Controls.Add(cmbPcModel)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label1)
+        Controls.Add(GroupBox2)
+        Controls.Add(txtLog)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "frmMain"
+        Text = "PT GUI"
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents txtLog As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents optWav As RadioButton
+    Friend WithEvents optMp3 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents optRename As RadioButton
+    Friend WithEvents optOverwrite As RadioButton
+    Friend WithEvents cmbPcModel As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents optFromFilename As RadioButton
+    Friend WithEvents optUseFilename As RadioButton
+    Friend WithEvents linkWeb As LinkLabel
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ SharpPocketToolsGUI/frmMain.vb
@@ -0,0 +1,321 @@
+﻿Imports System.ComponentModel
+Imports System.Formats.Tar
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class frmMain
+
+    Dim filesToPrcess As List(Of String)
+
+    Function ExecuteProcess(processName As String, processArgs As String, ByRef outStr As String, redirOut As Boolean, redirErr As Boolean) As Integer
+        Try
+            Dim myProcess As New Process()
+            myProcess.StartInfo.FileName = processName
+            myProcess.StartInfo.Arguments = processArgs
+
+            myProcess.StartInfo.UseShellExecute = False
+            myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            myProcess.StartInfo.CreateNoWindow = True
+
+            myProcess.StartInfo.RedirectStandardOutput = redirOut
+            myProcess.StartInfo.RedirectStandardError = redirErr
+
+            myProcess.Start()
+            outStr = "Launching external tool: " & processName & " " & processArgs & vbCrLf & vbCrLf
+
+            If redirOut Then
+                outStr &= myProcess.StandardOutput.ReadToEnd
+            End If
+            If redirErr Then
+                outStr &= myProcess.StandardError.ReadToEnd
+            End If
+
+            myProcess.WaitForExit()
+
+            Return myProcess.ExitCode
+
+        Catch ex As Exception
+            outStr = "Fatal error when launching external tool: " & processName & " " & processArgs & vbCrLf & ex.ToString
+            Return -1
+        End Try
+
+    End Function
+
+    Sub ProcessFiles()
+        Dim ret As Integer = 0
+
+        For Each file In filesToPrcess
+            txtLog.Text &= "Processing " & file & vbCrLf & vbCrLf
+            If file.EndsWith(".bas", StringComparison.OrdinalIgnoreCase) Then
+                Dim sharpFileName As String
+                If optUseFilename.Checked = True Then
+                    sharpFileName = txtFileName.Text.ToUpper()
+                Else
+                    If Path.GetFileNameWithoutExtension(file).Length > txtFileName.MaxLength Then
+                        sharpFileName = Path.GetFileNameWithoutExtension(file).Substring(0, txtFileName.MaxLength).ToUpper()
+                        txtLog.Text &= "Warning! Filename is longer than " & txtFileName.MaxLength & " characters! It will be truncated to " & sharpFileName & vbCrLf & vbCrLf
+                    Else
+                        sharpFileName = Path.GetFileNameWithoutExtension(file).ToUpper()
+                    End If
+                End If
+                ret = ProcessBAS(file, sharpFileName)
+            ElseIf file.EndsWith(".wav", StringComparison.OrdinalIgnoreCase) Then
+                ret = ProcessWAV(file, False)
+            ElseIf file.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase) Then
+                ret = ProcessWAV(file, True)
+            End If
+            If ret = -1 Then
+                Exit For
+            End If
+            txtLog.Text &= "---------------------------------------------------------------------" & vbCrLf
+        Next
+
+        If ret = -1 Then
+            txtLog.Text &= vbCrLf & "Please check output above!" & vbCrLf
+        End If
+        txtLog.Text &= "Done." & vbCrLf
+
+        txtLog.SelectionStart = txtLog.Text.Length
+        txtLog.ScrollToCaret()
+    End Sub
+
+    Function ProcessBAS(fileName As String, sharpFileName As String)
+        Dim out As String = ""
+        Dim ret As Integer
+        Dim imgFile As String
+        Dim wavFile As String
+        Dim mp3File As String
+
+        My.Application.DoEvents()
+
+        imgFile = String.Concat(fileName.AsSpan(0, fileName.Length - 3), "IMG")
+
+        ret = ExecuteProcess("ptools\bas2img.exe", "--pc=" & cmbPcModel.Text & " """ & fileName & """ """ & imgFile & """", out, True, False)
+        txtLog.Text &= out & vbCrLf
+        If ret <> 0 Then
+            txtLog.Text &= vbCrLf & "bas2img returned error " & ret & "!" & vbCrLf
+            Return -1
+        End If
+
+        If Not My.Computer.FileSystem.FileExists(imgFile) Then
+            txtLog.Text &= vbCrLf & "bas2img failed to create image file: " & imgFile & vbCrLf
+            Return -1
+        End If
+
+        My.Application.DoEvents()
+
+        wavFile = Path.GetDirectoryName(fileName) & "\" & sharpFileName & ".WAV"
+        If optRename.Checked Then
+            If My.Computer.FileSystem.FileExists(wavFile) Then
+                Dim index As Integer = 1
+                Do While My.Computer.FileSystem.FileExists(Path.GetDirectoryName(fileName) & "\" & sharpFileName & " (" & index & ").WAV")
+                    index += 1
+                Loop
+                wavFile = Path.GetDirectoryName(fileName) & "\" & sharpFileName & " (" & index & ").WAV"
+            End If
+        End If
+
+        ret = ExecuteProcess("ptools\bin2wav.exe", "--type=img --pc=" & cmbPcModel.Text & " --name=" & sharpFileName & " """ & imgFile & """ """ & wavFile & """", out, True, False)
+        txtLog.Text &= out & vbCrLf
+        If ret <> 0 Then
+            txtLog.Text &= vbCrLf & "bin2wav returned error " & ret & "!" & vbCrLf
+            Return -1
+        End If
+
+        If Not My.Computer.FileSystem.FileExists(wavFile) Then
+            txtLog.Text &= vbCrLf & "bin2wav failed to create wave file: " & wavFile & vbCrLf
+            Return -1
+        End If
+
+        txtLog.Text &= "Deleting temporary img file: " & imgFile & vbCrLf & vbCrLf
+        My.Computer.FileSystem.DeleteFile(imgFile)
+
+        If optMp3.Checked Then
+            My.Application.DoEvents()
+            mp3File = String.Concat(wavFile.AsSpan(0, wavFile.Length - 3), "MP3")
+            If optRename.Checked Then
+                If My.Computer.FileSystem.FileExists(mp3File) Then
+                    Dim index As Integer = 1
+                    Do While My.Computer.FileSystem.FileExists(String.Concat(wavFile.AsSpan(0, wavFile.Length - 4), " (" & index & ").MP3"))
+                        index += 1
+                    Loop
+                    mp3File = String.Concat(wavFile.AsSpan(0, wavFile.Length - 4), " (" & index & ").MP3")
+                End If
+            End If
+            ret = ExecuteProcess("sox\sox.exe", """" & wavFile & """ -c 1 -C 128 """ & mp3File & """ rate 16000", out, False, True)
+            txtLog.Text &= out & vbCrLf
+            If ret <> 0 Then
+                txtLog.Text &= vbCrLf & "sox encoder returned error " & ret & "!" & vbCrLf
+                Return -1
+            End If
+            If Not My.Computer.FileSystem.FileExists(mp3File) Then
+                txtLog.Text &= vbCrLf & "sox encoder failed to create mp3 file: " & mp3File & vbCrLf
+                Return -1
+            End If
+            txtLog.Text &= "Deleting temporary wav file: " & wavFile & vbCrLf & vbCrLf
+            My.Computer.FileSystem.DeleteFile(wavFile)
+        End If
+
+        Return 0
+    End Function
+
+    Function ProcessWAV(fileName As String, isMp3 As Boolean)
+        Dim out As String = ""
+        Dim ret As Integer
+        Dim wavFile As String = fileName
+        Dim basFile As String
+
+        If isMp3 Then
+            My.Application.DoEvents()
+            wavFile = String.Concat(fileName.AsSpan(0, fileName.Length - 3), "WAV")
+            If optRename.Checked Then
+                If My.Computer.FileSystem.FileExists(wavFile) Then
+                    Dim index As Integer = 1
+                    Do While My.Computer.FileSystem.FileExists(String.Concat(fileName.AsSpan(0, fileName.Length - 4), " (" & index & ").WAV"))
+                        index += 1
+                    Loop
+                    wavFile = String.Concat(fileName.AsSpan(0, fileName.Length - 4), " (" & index & ").WAV")
+                End If
+            End If
+            ret = ExecuteProcess("sox\sox.exe", """" & fileName & """ -c 1 -b 8 """ & wavFile & """ rate 16000", out, False, True)
+            txtLog.Text &= out & vbCrLf
+            If ret <> 0 Then
+                txtLog.Text &= vbCrLf & "sox encoder returned error " & ret & "!" & vbCrLf
+                Return -1
+            End If
+            If Not My.Computer.FileSystem.FileExists(wavFile) Then
+                txtLog.Text &= vbCrLf & "sox encoder failed to create wav file: " & wavFile & vbCrLf
+                Return -1
+            End If
+        End If
+
+        basFile = String.Concat(wavFile.AsSpan(0, wavFile.Length - 3), "BAS")
+        If optRename.Checked Then
+            If My.Computer.FileSystem.FileExists(basFile) Then
+                Dim index As Integer = 1
+                Do While My.Computer.FileSystem.FileExists(String.Concat(wavFile.AsSpan(0, wavFile.Length - 4), " (" & index & ").BAS"))
+                    index += 1
+                Loop
+                basFile = String.Concat(wavFile.AsSpan(0, wavFile.Length - 4), " (" & index & ").BAS")
+            End If
+        End If
+
+        ret = ExecuteProcess("ptools\wav2bin.exe", "--pc=" & cmbPcModel.Text & " """ & wavFile & """ """ & basFile & """", out, True, False)
+        txtLog.Text &= out & vbCrLf
+        If ret <> 0 Then
+            txtLog.Text &= vbCrLf & "wav2bin returned error " & ret & "!" & vbCrLf
+            Return -1
+        End If
+
+        If Not My.Computer.FileSystem.FileExists(basFile) Then
+            txtLog.Text &= vbCrLf & "wav2bin failed to create bas file: " & basFile & vbCrLf
+            Return -1
+        End If
+
+        If isMp3 Then
+            txtLog.Text &= "Deleting temporary wav file: " & wavFile & vbCrLf & vbCrLf
+            My.Computer.FileSystem.DeleteFile(wavFile)
+        End If
+
+        Return 0
+    End Function
+
+
+    Private Sub frmMain_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        Dim files As String() = e.Data.GetData(DataFormats.FileDrop)
+        filesToPrcess.Clear()
+        txtLog.Clear()
+
+        For Each file In files
+            If file.EndsWith(".bas", StringComparison.OrdinalIgnoreCase) OrElse
+                file.EndsWith(".wav", StringComparison.OrdinalIgnoreCase) OrElse
+                file.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase) Then
+                filesToPrcess.Add(file)
+                txtLog.Text &= "Will process " & file & vbCrLf
+            Else
+                txtLog.Text &= "Unsupported " & file & vbCrLf
+            End If
+        Next
+
+        txtLog.Text &= "====================================================================" & vbCrLf & vbCrLf
+
+        ProcessFiles()
+    End Sub
+
+    Private Sub frmMain_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Text = My.Application.Info.Title & " v" & My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor
+        filesToPrcess = New List(Of String)
+
+        cmbPcModel.Text = My.Settings.pcModel
+
+        If My.Settings.outputFormat = 1 Then
+            optMp3.Checked = True
+        Else
+            optWav.Checked = True
+        End If
+        If My.Settings.filePolicy = 1 Then
+            optOverwrite.Checked = True
+        Else
+            optRename.Checked = True
+        End If
+        If My.Settings.fileNamePolicy = 1 Then
+            optUseFilename.Checked = True
+        Else
+            optFromFilename.Checked = True
+        End If
+
+        txtFileName.Text = My.Settings.sharpFileName
+    End Sub
+
+    Private Sub optUseFilename_CheckedChanged(sender As Object, e As EventArgs) Handles optUseFilename.CheckedChanged
+        If optUseFilename.Checked = True Then
+            txtFileName.Enabled = True
+        End If
+    End Sub
+
+    Private Sub optFromFilename_CheckedChanged(sender As Object, e As EventArgs) Handles optFromFilename.CheckedChanged
+        If optFromFilename.Checked = True Then
+            txtFileName.Enabled = False
+        End If
+    End Sub
+
+    Private Sub cmbPcModel_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbPcModel.SelectedIndexChanged
+        If cmbPcModel.Text = "1500" Then
+            txtFileName.MaxLength = 16
+        Else
+            txtFileName.MaxLength = 7
+        End If
+    End Sub
+
+    Private Sub frmMain_Closing(sender As Object, e As CancelEventArgs) Handles MyBase.Closing
+        My.Settings.pcModel = cmbPcModel.Text
+
+        If optMp3.Checked Then
+            My.Settings.outputFormat = 1
+        Else
+            My.Settings.outputFormat = 0
+        End If
+        If optOverwrite.Checked Then
+            My.Settings.filePolicy = 1
+        Else
+            My.Settings.filePolicy = 0
+        End If
+        If optUseFilename.Checked Then
+            My.Settings.fileNamePolicy = 1
+        Else
+            My.Settings.fileNamePolicy = 0
+        End If
+
+        My.Settings.sharpFileName = txtFileName.Text
+    End Sub
+
+    Private Sub linkWeb_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkWeb.LinkClicked
+        Process.Start("https://github.com/SilverGreen93")
+    End Sub
+End Class
